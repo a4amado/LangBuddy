7897517e8bb2941a5c59459cd54dd5b8a0616752
Ahmad Addel <a4amado@gmail.com>
2024-12-27 01:51:04 +0200

    Update create-post.tsx

--- src/app/_components/post/create-post.tsx
+++ src/app/_components/post/create-post.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button, Input, Modal } from "antd";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { api } from "~/trpc/react";
@@ -9,12 +10,13 @@ import { UploadDropzone } from "~/utils/uploadthing";
 export default function CreatePost() {
     return (
         <div className="my-2">
+            <Link href="/posts/create">
             <Input.TextArea
                 className="text-lg"
                 autoSize={{ minRows: 2, maxRows: 2 }}
                 role="button"
                 placeholder="Ask Something!"
-            />
+            /></Link>
         </div>
     );
 }
