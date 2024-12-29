9bf34448ae0a12b1de6e246af5a38b0126e016be
Ahmad Addel <a4amado@gmail.com>
2024-12-30 01:59:08 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
-"use client";
 import "~/styles/globals.css";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
 import { Inter } from "next/font/google";
+import { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
-
+export const metadata: Metadata = {
+    openGraph: {
+      images: [{
+        url: 'https://lang-buddy.vercel.app/langbuddy.webp', // Replace with your actual domain
+      }],
+    },
+  };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
